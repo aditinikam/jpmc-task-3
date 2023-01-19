2ddf69bc15f1d221cf1069173c359352cb58bede
aditinikam <aditirnikam21@gmail.com>
2023-01-19 19:16:32 +0530

    added live graph that displays the data feed in a clear and visually appealing way for traders to monitor

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: Number,
+  price_def: Number,
+  ratio: Number,
+  lower_bound: Number,
+  upper_bound: Number,
   timestamp: Date,
+  trigger_alert: Number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const priceABC = (serverRespond[0].top_ask.price+serverRespond[0].top_bid.price)/2;
+    const priceDEF = (serverRespond[1].top_ask.price+serverRespond[1].top_bid.price)/2;
+    const ratio = priceABC/priceDEF;
+    const upper_bound = 1+0.05;
+    const lower_bound = 1-0.05;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      lower_bound,
+      upper_bound,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp : serverRespond[1].timestamp,
+      trigger_alert: (ratio>upper_bound || ratio<lower_bound)? ratio:undefined,
+    }
   }
 }
